47e3e0f6c93d7a72da490fe53af2754f1c41f7ff
25ramy <ramylinares@gmail.com>
2022-11-17 18:50:32 -0300

    mp cont

--- src/controllers/pagompController.js
+++ src/controllers/pagompController.js
@@ -1,25 +1,28 @@
 const { response } = require("express");
+const mercadopago = require('mercadopago');
+
+mercadopago.configure({
+    access_token: 'TEST-359702488802332-110617-1827c3c7bf4214710cc8dcdc10d14fca-136098123'
+});
 
 const postPagoMP = async (req, res = response) => {
   try {
     const { items } = req.body;
-    var mercadopago = require('mercadopago');
-    mercadopago.configure({
-        access_token: 'TEST-359702488802332-110617-1827c3c7bf4214710cc8dcdc10d14fca-136098123'
-    });
+
     
     var preference = {
 items
     };
     
-    await mercadopago.preferences.create(preference)
-    const msg=items.map(it=>
+    const response=await mercadopago.preferences.create(preference)
+   /*  const msg=items.map(it=>
         `pago : ${it.quantity} ${it.title} de $${it.unit_price}, Realizado!`
     )
     res.status(200).json({
         status: true,
         msg: msg.join(','),
-      });
+      }); */
+      res.redirect(response.body.init_point)
     }
    catch (error) {
     console.log(error);
